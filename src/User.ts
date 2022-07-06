0641e4589595e988b63f508442ef97fa1f278be5
Belousov, Dmitrii <beloudmi@fel.cvut.cz>
2022-07-06 17:06:13 +0200

    usdmClientCompete

--- src/User.ts
+++ src/User.ts
@@ -1,11 +1,17 @@
 import { isWsFormattedFuturesUserDataEvent } from 'binance'
 import { WsClientExd } from './wsClientExd';
 import { UsdmClientExd } from './usdmClientExd'
+import { OrderData, UserData } from './types';
+import { OrderBuilder } from './orderBuilder';
 
 export class User {
 
     private wsClient: WsClientExd;
     private usdmClient: UsdmClientExd;
+    private orderBuilder: OrderBuilder;
+    private userData: UserData;
+
+    private isLiquidationStreamLocked: boolean;
 
     constructor(data: UserData) {
 
@@ -18,25 +24,44 @@ export class User {
         this.usdmClient = new UsdmClientExd({
             api_key: data.api_key,
             api_secret: data.api_secret,
-        })
+        }, undefined, data.isTestnet)
+
+        this.orderBuilder = new OrderBuilder(this.usdmClient);
 
         this.wsClient.subscribeAllLiquidationOrders("usdm", false);
         this.wsClient.subscribeUsdFuturesUserDataStream(data.isTestnet, true, true);
+
+        this.userData = data;
+
+        this.isLiquidationStreamLocked = false;
 
         this.start();
 
     }
 
     private start() {
-        this.wsClient.on("formattedMessage", (msg) => {
+        this.wsClient.on("formattedMessage", async (msg) => {
 
             if(isWsFormattedFuturesUserDataEvent(msg)) {
 
-                console.log("Formated futures user data event")
+                console.log("user data")
 
-            } else if("eventType" in msg){
+            } else if("eventType" in msg) {
 
-                console.log(msg.eventType)
+                if("liquidationOrder" in msg && !this.isLiquidationStreamLocked) {
+
+                    console.log("... here ")
+                    this.isLiquidationStreamLocked = true;
+
+                    const data: OrderData = await this.orderBuilder.setDataForOrders(msg.liquidationOrder.symbol,
+                        this.userData.percent, this.userData.leverage);
+
+                    await this.usdmClient.openPosition(this.orderBuilder.getMarketOrder(data));
+                    for(let i = 1; i <= 9; i++) await this.usdmClient.createLimitOrder(this.orderBuilder.getLimitOrder(data, i));
+
+                } else {
+                    console.log(msg);
+                }
 
             }
 
--- src/orderBuilder.ts
+++ src/orderBuilder.ts
@@ -1,44 +1,56 @@
-import { USDMClient } from "binance";
 import { LimitOrderData, MarketOrderData, OrderData } from "./types";
+import { UsdmClientExd } from "./usdmClientExd";
 import { Utils } from "./utils";
 
 export class OrderBuilder {
 
     private utils: Utils;
 
-    constructor(usdmClient: USDMClient) {
+    constructor(usdmClient: UsdmClientExd) {
         this.utils = new Utils(usdmClient);
     }
 
-    public async getMarketOrder(symbol: string, percent: number, leverage: number): Promise<MarketOrderData> {
-        const available_balance = await this.utils.getAvailableUSDTBalance();
-        const pricePrecision = await this.utils.getPrecisionPrice(symbol);
-        const price = await this.utils.getPrice(symbol, pricePrecision);
-        const precision = await this.utils.getPrecision(symbol);
-        const quantity = await this.utils.getQuantity(available_balance, price, precision, percent, leverage);
+    public getMarketOrder(data: OrderData): MarketOrderData {
+        
+        const quantity_ten = this.utils.floorPrecised(data.quantity/10.0, data.precision);
 
         return {
             side: "SELL",
-            symbol: symbol,
-            quantity: quantity,
+            symbol: data.symbol,
+            quantity: quantity_ten,
             reduceOnly: "false"
         }
 
     }
 
-    public async getLimitOrder(symbol: string, percent: number, leverage: number): Promise<LimitOrderData>{
+    public getLimitOrder(data: OrderData, pos: number): LimitOrderData{
+
+        const priceForOrder = this.utils.floorPrecised(data.price + data.step*pos, data.pricePrecision);
+        const quantity_ten = this.utils.floorPrecised(data.quantity/10.0, data.precision);
+
+        return {
+            side: "SELL",
+            symbol: data.symbol,
+            price: priceForOrder,
+            quantity: quantity_ten,
+            reduceOnly: "false"
+        }
+    }
+
+    public async setDataForOrders(symbol: string, percent: number, leverage: number): Promise<OrderData> {
         const available_balance = await this.utils.getAvailableUSDTBalance();
         const pricePrecision = await this.utils.getPrecisionPrice(symbol);
         const price = await this.utils.getPrice(symbol, pricePrecision);
         const precision = await this.utils.getPrecision(symbol);
-        const quantity = await this.utils.getQuantity(available_balance, price, precision, percent, leverage);
-
+        const quantity = this.utils.getQuantity(available_balance, price, precision, percent, leverage);
+        const step = 0.1/leverage*price;
         return {
-            side: "SELL",
-            symbol: "BTCUSDT",
+            symbol: symbol,
+            quantity: quantity,
             price: price,
-            quantity: 1,
-            reduceOnly: "false"
+            pricePrecision: pricePrecision,
+            step: step,
+            precision: precision
         }
     }
 
