47572e239fa5aca2079f405dc88bd678705dacf8
Dima <57431410+FishRoyal@users.noreply.github.com>
2022-07-08 21:49:29 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { User } from "./User"
 
+//I left here my testnet apikey as an example
 const user = new User({
     api_key: "9ec08cae0e9952bca21524b2fe7cfa82b9931b472231ae05eb122a12631c4b69",
     api_secret: "0799e705b9e504e5229a44e61b2f07974c83e04e5f83393ed5ead41ca12874d0",
